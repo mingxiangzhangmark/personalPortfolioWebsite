c4e82912b17ed04e756031ce7e29cded096dbbc7
mingxiangzhang_mark@outlook.com <mingxiangzhang_mark@outlook.com>
2024-04-01 18:36:53 +1100

    update stats page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     
     $('#slides').superslides({
@@ -32,22 +33,63 @@ $(document).ready(function(){
         }
     });
 
-    $('.chart').easyPieChart({
-        easing: 'easeInOut',
-        barColor: '#fff',
-        trackColor:false,
-        scaleColor:false,
-        lineWidth:4,
-        size:152,
-        onStep:function(from, to , persent){
-            $(this.el).find('.percent').text(Math.round(persent));
-        }
-        //your options goes here
-    });
     
-   
+    
+
+    // var skillsTopOffSet = $(".skillsSection").offset().top;
+    // // console.log(skillsTopOffSet);
+    // $(window).scroll(function(){
+    //     if(window.scrollY > skillsTopOffSet - $(window).height()+200) {
+    //         // Add your code here
+    //     }
+    // });
+
+    var skillsTopOffSet = $(".skillsSection").offset().top;
+    var statsTopOffSet = $(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    console.log(skillsTopOffSet); // Debugging: Check the offset value
+
+    $(window).scroll(function() {
+        if(window.scrollY > skillsTopOffSet - $(window).height() + 200) {
+            // Add your code here
+            //console.log('Skills section is now visible'); // Test scrolling action
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor:false,
+                scaleColor:false,
+                lineWidth:4,
+                size:152,
+                onStep:function(from, to , persent){
+                    $(this.el).find('.percent').text(Math.round(persent));
+                }
+                //your options goes here
+            });
+
+        }
+    });
+    $(window).scroll(function() {
+    if(! countUpFinished && window.scrollY > statsTopOffSet - $(window).height() + 200) {
+        $(".counter").each(function(){
+            var element = $(this);
+            var endVal = parseInt(element.text());
+            element.countup(endVal);
+        });
+            countUpFinished = true;
+        }
+    });
+
+
+
+
 
 });
+
+    
+    
+
+
 
 
 
