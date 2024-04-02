0f3605cea7ada91637efc7a37a8115ae00977248
mingxiangzhang_mark@outlook.com <mingxiangzhang_mark@outlook.com>
2024-04-02 23:50:36 +1100

    final version not add my own work

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This is my self portfolio website, mainly use Bootstrap, JQuery, HTML,CSS3 and J
 Pictures:
 
 ![alt text](image.png)
-
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+$(window).on("load",function () {
+    $(".loader").fadeOut(1500);
+
+    $("items").isotope({
+        filter:"*",
+        animationOptions:{
+            duration:1500,
+            easing:'linear',
+            queue:false,
+        }
+    })
+});
+
+
 
 $(document).ready(function(){
     
@@ -66,7 +80,6 @@ $(document).ready(function(){
                 }
                 //your options goes here
             });
-
         }
     });
     $(window).scroll(function() {
@@ -80,9 +93,54 @@ $(document).ready(function(){
         }
     });
 
+    // $("[data-fancybox]").fancybox();
+    $("[data-fancybox]").fancybox();
 
+    // $("items").isotope({
+    //     filter:"*",
+    //     animationOptions:{
+    //         duration:1500,
+    //         easing:'linear',
+    //         queue:false,
+    //     }
+    // })
 
+    $("#filters a").click(function() {
 
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
+    const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
 
 });
 
